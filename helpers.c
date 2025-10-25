a542d7719e12cd5d0c0a9e99b73fab7d88f2cfa0
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-10-25 11:53:10 +0200

    Mad hacks. Tror jeg må begynne på nytt på implementasjonen av løkker, ellers tror jeg dette er fine.

--- helpers.c
+++ helpers.c
@@ -43,3 +43,121 @@ char *initBrainFuckMemory(int numBytes)
     }
     return memPtr;
 }
+
+struct Node
+{
+    char data;
+    struct Node *next;
+};
+struct Node *newLinkedList()
+{
+    struct Node *root = malloc(sizeof(struct Node));
+    if (root == NULL)
+    {
+        printf("Unable to allocate memory for new LL node\n");
+        exit(1);
+    }
+    root->next = NULL;
+    return root;
+}
+
+int getListLength(struct Node *root)
+{
+    struct Node *ptr = root;
+    int len = 0;
+    while (ptr != NULL)
+    {
+        ptr = ptr->next;
+        len++;
+    }
+    printf("Found len %d\n", len);
+    return len;
+}
+
+struct Node *getPenultimateNode(struct Node *root)
+{
+    struct Node *ptr = root;
+    struct Node *prev = NULL;
+    while (ptr != NULL)
+    {
+        prev = ptr;
+        ptr = ptr->next;
+    }
+    return prev;
+}
+
+struct Node *appendToLinkedList(struct Node *root, char data)
+{
+    struct Node *penultimate = getPenultimateNode(root);
+
+    struct Node *tail = penultimate->next;
+
+    struct Node *newNode = newLinkedList();
+    if (tail == NULL)
+    {
+        penultimate->next = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+    }
+
+    return newNode;
+}
+
+void printLinkedList(struct Node *root)
+{
+    struct Node *ptr = root;
+    printf("Linkedlist with root @ %x: { ", root);
+    while (ptr != NULL)
+    {
+        char data = ptr->data;
+        printf(" %d ", data);
+
+        ptr = ptr->next;
+    }
+    printf(" }\n");
+}
+
+char popLinkedList(struct Node *root)
+{
+    int len = getListLength(root);
+    // TODO: Is handling this case needed? Why not simply never pop empty lists?
+    if (len == 0)
+    {
+        printf("\tWARN: Popped from empty list\n");
+        return 0;
+    }
+    else if (len == 1)
+    {
+        char ret = root->data;
+        free(root);
+        return ret;
+    }
+    // At this point, length must be at least 2
+
+    // First, find final and penultimate nodes
+    struct Node *penultimate = getPenultimateNode(root);
+    printf("Penultimate is @ mem adr %x\n", penultimate);
+    struct Node *oldTail = penultimate->next;
+    printf("Tail is @ mem adr %x\n", oldTail);
+
+    if (oldTail == NULL || penultimate == NULL)
+    {
+        printf("Critical error in linked list popping :/\n");
+        printLinkedList(root);
+    }
+
+    printf("Found pen and old tail\n");
+
+    // Pop last from list. Decouple "oldTail".
+    printf("Wiping 'next' of penultimate node\n");
+    penultimate->next = NULL;
+    printf("Persisting data from tail\n");
+    char ret = oldTail->data;
+    printf("Freeing tail\n");
+    free(oldTail);
+    printf("Popped finely :)\n");
+    // Return removed data
+    return ret;
+}
--- main.c
+++ main.c
@@ -7,24 +7,11 @@
 #define MEMORY_BYTES 30000 // 30_000
 #define DEBUG 1
 
-// TODO: Its probably possible to do this more efficiently. But as n is only 8, its probably fine.
-char charIsInLanguage(char c)
+void printCurrentMemory(char *bfMemory, int memIdx)
 {
-    // BF only supports 8 operators. We will ignore all other chars.
-    // https://gist.github.com/roachhd/dce54bec8ba55fb17d3a -> <>+-[],.
-    char supportedChars[8] = {'<', '>', '+', '-', '[', ']', ',', '.'};
-    for (int i = 0; i < 8; i++)
-    {
-        if (supportedChars[i] == c)
-        {
-            // If we find a match, we instantly return.
-            return 1;
-        }
-    }
-    return 0;
+    printf("Current memory is %d, at idx %d\n", bfMemory[memIdx], memIdx);
 }
-
-void interpretBfSource(char *sourceCode, char *memory)
+void interpretBfSource(char *sourceCode, char *bfMemory)
 {
     int sourceLen = strlen(sourceCode);
     if (DEBUG)
@@ -33,60 +20,92 @@ void interpretBfSource(char *sourceCode, char *memory)
     }
 
     int memIdx = 0;
+    int loopStartIdx = -1;
+    char curVal;
+
+    // We represent BF loops with symmetric lists, where each index in the lists correspond to loop indeces.
+    struct Node *loopStarts = NULL; // Only instanciate the list if we have data for it.
+    struct Node *loopEnds = NULL;   // Only instanciate the list if we have data for it.
+
     for (int i = 0; i < sourceLen; i++)
     {
-        char c = sourceCode[i];
-        if (!charIsInLanguage(c))
-        {
-            if (DEBUG)
-                printf("Skipping non-supported char %c\n", c);
-            // Simply ignore non-supported chars.
-            continue;
-        }
+        // Not strictly an operator, might also be ignored symbol
+        char operator = sourceCode[i];
+        if (DEBUG)
+            printf("Char is %c\n", operator);
 
-        switch (c)
+        switch (operator)
         {
 
         case '<': // < decreases memory pointer, or moves the pointer to the left 1 block.
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            // TODO: How should we handle wrapping around the memory?
+            memIdx = (memIdx - 1) % MEMORY_BYTES;
             break;
         case '>': // > increases memory pointer, or moves the pointer to the right 1 block.
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            // TODO: How should we handle wrapping around the memory?
+            memIdx = (memIdx + 1) % MEMORY_BYTES;
             break;
         case '+': // + increases value stored at the block pointed to by the memory pointer
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            bfMemory[memIdx]++;
             break;
         case '-': // - decreases value stored at the block pointed to by the memory pointer
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            bfMemory[memIdx]--;
             break;
         case '[': // [ like c while(cur_block_value != 0) loop.
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            printCurrentMemory(bfMemory, memIdx);
+
+            printf("Resultatet er: %c\n", bfMemory[memIdx] != 0);
+
+            if (bfMemory[memIdx] != 0)
+            {
+
+                printf("Mem is not 0, init loop\n");
+                if (loopStarts == NULL)
+                {
+                    loopStarts = newLinkedList();
+                    loopStarts->data = memIdx;
+                }
+                else
+                {
+                    appendToLinkedList(loopStarts, memIdx);
+                }
+            }
+            else
+            {
+                printf("No loop, will pop lists\n");
+                popLinkedList(loopStarts);
+                popLinkedList(loopEnds);
+            }
+            printf("Breaker\n");
             break;
         case ']': // ] if block currently pointed to's value is not zero, jump back to [
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            if (bfMemory[memIdx] != 0)
+            {
+                printf("Mem is not 0, loop back\n");
+                struct Node *jumpTo = getPenultimateNode(loopStarts);
+                memIdx = jumpTo->data;
+            }
+            else
+            {
+                printf("Mem IS 0, pop loop lists\n");
+                popLinkedList(loopStarts);
+                popLinkedList(loopEnds);
+            }
             break;
         case ',': // , like c getchar(). input 1 character.
-            if (DEBUG)
-                printf("Switch case for char %c\n", c);
+            bfMemory[memIdx] = getchar();
             break;
         case '.': // . like c putchar(). print 1 character to the console
+            char memVal = bfMemory[memIdx];
             if (DEBUG)
-                printf("Switch case for char %c\n", c);
-            char memVal = memory[memIdx];
-            printf("Memval is %c\n", memVal);
+                printCurrentMemory(bfMemory, memIdx);
             putchar(memVal);
             break;
 
+        // Simply ignore non-supported chars.
         default:
-            // TODO: Do we really need to 'continue' above? How about just having this default be a NOP?
-            printf("Illegal state in char switch statement for char %c\n", c);
-            exit(1);
+            if (DEBUG)
+                printf("Ignoring char %c\n", operator);
         }
     }
 }
