390671cb89ea00f2e24bcd86cd25c10abd9b8a5f
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-11-02 19:31:44 +0100

    Cleanup.

--- main.c
+++ main.c
@@ -3,14 +3,9 @@
 #include <string.h>
 #include "helpers.c"
 
-#define LINE_LENGTH 300
 #define MEMORY_BYTES 30000 // 30_000
 #define DEBUG 0
 
-void printCurrentMemory(char *bfMemory, int memIdx)
-{
-    printf("Current memory is %d, at idx %d\n", bfMemory[memIdx], memIdx);
-}
 void interpretBfSource(char *sourceCode, char *bfMemory)
 {
     int sourceLen = strlen(sourceCode);
@@ -72,10 +67,8 @@ void interpretBfSource(char *sourceCode, char *bfMemory)
     int memIdx = 0;
     int instructionIndex = 0;
 
-    // for (int i = 0; i < sourceLen; i++)
-    while (instructionIndex < sourceLen)
+    do
     {
-
         if (instructionIndex < 0)
         {
             printf("CRITICAL: Illegal state, instruction pointer is %d\n", instructionIndex);
@@ -90,16 +83,11 @@ void interpretBfSource(char *sourceCode, char *bfMemory)
 
         switch (operator)
         {
-
         case '<': // < decreases memory pointer, or moves the pointer to the left 1 block.
-            // TODO: How should we handle wrapping around the memory?
             memIdx--;
-            // memIdx = (memIdx - 1) % MEMORY_BYTES;
             break;
         case '>': // > increases memory pointer, or moves the pointer to the right 1 block.
-            // TODO: How should we handle wrapping around the memory?
             memIdx++;
-            // memIdx = (memIdx + 1) % MEMORY_BYTES;
             break;
         case '+': // + increases value stored at the block pointed to by the memory pointer
             bfMemory[memIdx]++;
@@ -108,46 +96,18 @@ void interpretBfSource(char *sourceCode, char *bfMemory)
             bfMemory[memIdx]--;
             break;
         case '[': // [ like c while(cur_block_value != 0) loop.
-            if (DEBUG)
-                printCurrentMemory(bfMemory, memIdx);
-
-            if (DEBUG)
-                printf("Resultatet er: %d\n", bfMemory[memIdx] != 0);
-
             if (bfMemory[memIdx] == 0)
-            {
-                if (DEBUG)
-                    printf("No loop\n");
                 instructionIndex = jumpMap[instructionIndex];
-                // instructionIdxStack = popStack(instructionIdxStack);
-            }
-            if (DEBUG)
-                printf("Breaker\n");
             break;
         case ']': // ] if block currently pointed to's value is not zero, jump back to [
             if (bfMemory[memIdx] != 0)
-            {
-                if (DEBUG)
-                    printf("Mem is not 0 (%d), loop back\n", bfMemory[memIdx]);
                 instructionIndex = jumpMap[instructionIndex];
-                if (DEBUG)
-                    printf("Set instruciton idx to %d\n", instructionIndex);
-            }
-            else
-            {
-                // TODO: Denne skal HOPPE FREM til sammensvarende ]
-                if (DEBUG)
-                    printf("Mem IS 0, pop jump stack\n");
-            }
             break;
         case ',': // , like c getchar(). input 1 character.
             bfMemory[memIdx] = getchar();
             break;
         case '.': // . like c putchar(). print 1 character to the console
-            char memVal = bfMemory[memIdx];
-            if (DEBUG)
-                printf("Putchar: %d/0x%x\n", memVal, memVal);
-            putchar(memVal);
+            putchar(bfMemory[memIdx]);
             break;
 
         // Simply ignore non-supported chars.
@@ -155,11 +115,7 @@ void interpretBfSource(char *sourceCode, char *bfMemory)
             if (DEBUG)
                 printf("Ignoring char %c\n", operator);
         }
-        if (!dontIncrementIpr)
-            instructionIndex++;
-        if (instructionIndex >= sourceLen)
-            break;
-    }
+    } while (instructionIndex++ < sourceLen);
 }
 
 int main(int argc, char **argv)
